bec80ec0041b44a28ca2a8d6d30d4ae472071907
Jhanvi-Lulla <jhanvilulla11@gmail.com>
2025-06-13 13:27:34 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+class expense(models.Model):
+    def __str__(self):
+        return f'Expense:{self.Expense_reason}.'
+    
+    Expense_reason=models.CharField(max_length=100)
+    Amount=models.FloatField(0)
+    Image=models.CharField(max_length=500,null=True, default="https://www.lystloc.com/blog/wp-content/uploads/2023/02/How-To-Manage-Field-Employee-Expenses-1.webp")
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    salary= models.IntegerField(default=0)
+
+    
 
 # Create your models here.
--- views.py
+++ views.py
@@ -1,43 +1,136 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
+from django.shortcuts import render,redirect
 from tracker.models import expense
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from .forms import ItemForm, ItemFormModel
+from django.db.models import Sum
 
 
-def register(request):
+#------------------------index------------------------------
+def index(request):
+    expense_list=expense.objects.all()
+    data={
+        'expense_list': expense_list,
+    }
+    return render(request,'tracker/index2.html',data)
+#------------------------first page "welcome"------------------------------
+def welcome(request):
+    return render(request,'tracker/welcome.html')
+#------------------------detail "for seeing 1 item "------------------------------
+def detail(request,expense_id):
+    obj=expense.objects.get(pk=expense_id)
+    data={
+        'obj': obj,
+    }
+    return render(request,'tracker/detail.html',data)
+#------------------------delete------------------------------
+def delete_item(request,expense_id):
+    obj=expense.objects.get(id=expense_id)
+    if(request.method=="POST"):
+        obj.delete()
+        return redirect('index')
+    return render(request, 'tracker/delete_item.html',{'obj': obj})
+#------------------------add------------------------------
+def add_item(request):
     if request.method=="POST":
-        form=UserCreationForm(request.POST)
+        Expense_reason=request.POST.get('Expense_reason')
+        Amount=request.POST.get('Amount')
+        Image=request.POST.get('Image')
+        print("Expense: ",Expense_reason)
+        print("in if part")
+        obj=expense(Expense_reason=Expense_reason, Amount=Amount, Image=Image)
+    
+        obj.save()
+        form=ItemForm(request.POST)
+        return redirect('index')
+    else:
+        print("in else part")
+        form=ItemForm()
+        data={'form': form }
+        return render(request, 'tracker/addItem.html', data)
+#------------------------update------------------------------
+def update_item(request,expense_id):
+    item=expense.objects.get(id=expense_id)
+    form=ItemFormModel(request.POST or None , instance=item)
+    if request.method=="POST":
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            print(username)
-            str= f'Welcome {username} !! Your Account has been created successfully!'
             form.save()
-            messages.success(request, str)
-        return redirect('login')
-    else:
-        form=UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
-def user_posts(request):
-    # Get the logged-in user (request.user will contain the logged-in user)
-    user = request.user
-    if user.is_authenticated:
-        # Retrieve posts for the logged-in user
-        posts = expense.objects.filter(created_by=user)
-        print(posts,"--------------------------------")
-        return render(request, 'tracker/index2.html', {'posts': posts})
-    else:
-        # If user is not authenticated, redirect to login page or show a message
-        return render(request, 'users/login.html')
-def custom_logout(request):
-    logout(request)
-    return render(request,'users/logout.html')
+            return redirect('index')
+    data={'form': form,
+        'item1': item}
+    return render(request,'tracker/addItem.html', data)
+
+def unique_user(request):
+    salary=0
+    if request.method == "POST":
+        data=request.POST
+        salary=int(data.get('salary', 0))
+        Expense_reason=data.get('Expense_reason')
+        Amount= int(data.get('Amount', 0))
+        Image= data.get('Image')
+        
+        expense.objects.create(
+            user=request.user,
+            salary=salary,
+            Amount=Amount,
+            Expense_reason=Expense_reason,
+            Image=Image, 
+        )
+        return redirect('index')
+    queryset= expense.objects.filter(user=request.user.id)
+    if request.GET.get('search'):
+        queryset = queryset.filter(
+            name__icontains=request.GET.get('search')
+        )
+    total_sum= sum(exp.Amount for exp in queryset)
+    context={'expenses': queryset, 'total_sum': total_sum }
+    return render(request, 'tracker/index2.html', context )
+def pdf(request):
+    if request.method=='POST':
+        data= request.POST
+        salary= int(data.get('salary'))
+        Expense_reason=data.get('Expense_reason')
+        Amount= int(data.get('Amount', 0))
+        Image= data.get('Image')
+        print("user in create",request.user)
+        expense.objects.create(
+            user=request.user,
+            salary=salary,
+            Amount=Amount,
+            Expense_reason=Expense_reason,
+            Image=Image, 
+            
+        )
+        return redirect('pdf')
+    print("username",request.user.username)
+    print("userid",request.user.id)
+    queryset= expense.objects.filter(user=request.user.id)
+    if request.GET.get('search'):
+        queryset = queryset.filter(
+            name__icontains=request.GET.get('search')
+        )
+    print("QuerySet",queryset)
+    user = request.POST.get('user')  
+    total_sum= expense.objects.filter(user=user).aggregate(total=Sum('Amount'))['total'] or 0
+    print("total sum",total_sum)
+    username= request.user.username
+    context={'expenses':queryset, 'total_sum': total_sum, 'username': username }
+    return render(request, 'tracker/pdf.html', context)
+
+
+
     
 
 
 
 
+    
+
         
+
+
+    
+
+    
+
+
 
 # Create your views here.
